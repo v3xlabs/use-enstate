69623f2abddafd4881d5436a19f78728fcca418f
Jakob Helgesson <jakob.helgesson@gmail.com>
2024-03-02 15:07:13 +0100

    Fix exports and allow helpers import

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entryPoints: ['src/index.ts'], // Adjust the entry point based on your project structure
+    entry: ['src/index.ts', 'src/helpers.ts'], // Adjust the entry point based on your project structure
     format: ['cjs', 'esm'], // Generate CommonJS and ECMAScript Modules bundles
     dts: true, // Generate TypeScript declaration files (*.d.ts)
     outDir: 'dist', // Output directory for the generated bundles
-    minify: true, // Optionally, enable minification
     sourcemap: true, // Generate sourcemaps for easier debugging
+    splitting: true,
+    clean: true,
+    minify: true, // Optionally, enable minification
+    minifyIdentifiers: true,
+    minifyWhitespace: true,
+    minifySyntax: true,
 });
